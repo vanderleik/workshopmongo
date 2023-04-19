a9476bc2f26cdb2db1c2ba3cd2f7bae8365ebdba
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-04-19 14:37:15 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/vanderleik/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,21 @@
+package com.vanderleik.workshopmongo.resources.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class StandardError {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
