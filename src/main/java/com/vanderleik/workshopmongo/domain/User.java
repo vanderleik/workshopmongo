3aac08e67f2384045a19d84e38022b006d700362
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-04-20 13:42:08 -0300

    Referenciando os posts do usuário

--- src/main/java/com/vanderleik/workshopmongo/domain/User.java
+++ src/main/java/com/vanderleik/workshopmongo/domain/User.java
@@ -2,9 +2,12 @@ package com.vanderleik.workshopmongo.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,12 +23,12 @@ public class User implements Serializable {
     private String name;
     private String email;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
 }
